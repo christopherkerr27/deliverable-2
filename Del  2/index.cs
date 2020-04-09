69f56c468ae3a1f02f0712f4cf25f8deab40e67b
christopherkerr27 <56808327+christopherkerr27@users.noreply.github.com>
2020-04-09 12:42:46 -0400

    Add files via upload

--- Del  2/index.cs
+++ Del  2/index.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Text;
 
 public class Program
 {
@@ -7,21 +6,18 @@ public class Program
 	{
 		Console.WriteLine("What is your message?");
 		string input = Console.ReadLine();
-		string choice = input.ToUpper();
-		string message = choice;
-		foreach (char c in choice)
-        {
-            Console.WriteLine(c);
-        }
-        for (int i = 0; i < choice.Length; i++)
-        {
-            Console.WriteLine(choice[i]);
-        }
-{
-  Console.WriteLine(i);
-}
-        
-		
-	
+		input = input.ToUpper();
+		int checksum = 0;
+		string message = "";
+		foreach (char letter in input)
+		{
+			string messageLetter = (Convert.ToInt32(letter) - 64).ToString();
+			message = message + messageLetter + "-";
+			var str = messageLetter;
+			checksum = checksum + (Convert.ToInt32(letter) - 64);
+		}
+
+		Console.WriteLine("Encoded message is " + message);
+		Console.WriteLine("The checksum is " + checksum);
 	}
 }
